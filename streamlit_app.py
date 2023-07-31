bd6af5daf612fbf871a2090cff947f9cca5a0ff8
SnowflakeZKInterm <140736730+SnowflakeZKInterm@users.noreply.github.com>
2023-07-31 15:26:31 -0400

    Update streamlit_app.py
    Added a fruit picker box and changed the indexing to be based on the fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,10 @@ streamlit.text('🥑🍞 Avocado Toast')
 #Special Header Smoothie
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+# Display the table on the page.
 #Creating a pandas dataframe and display for it
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+# Pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
