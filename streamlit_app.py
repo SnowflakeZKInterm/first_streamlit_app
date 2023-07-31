676d68afd880071ea8688b1726a06ab719d67234
SnowflakeZKInterm <140736730+SnowflakeZKInterm@users.noreply.github.com>
2023-07-31 15:10:33 -0400

    Update streamlit_app.py
    Change to actual comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner!')
 
-//Breakfast menu
+---Breakfast menu
 streamlit.heading('Breakfast menu')
 streamlit.paragraph('Omega 3 & Blueberry Oatmeal')
 streamlit.paragraph('Kale, Spinach & Rocket Smoothie')
