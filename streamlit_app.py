27cf2227d6dfb4796b44f3f1f84a8e7e9a96c276
SnowflakeZKInterm <140736730+SnowflakeZKInterm@users.noreply.github.com>
2023-07-31 15:39:56 -0400

    Update streamlit_app.py
    Changed display frame to the new table

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
   # Display the table on the page.
   #Creating a pandas dataframe and display for it
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
- #my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
   # Pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
   #Dataframe display
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
