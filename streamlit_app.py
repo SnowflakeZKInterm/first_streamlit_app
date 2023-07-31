72ea3b789df050fcf5e4a010a72bad749c036c7d
SnowflakeZKInterm <140736730+SnowflakeZKInterm@users.noreply.github.com>
2023-07-31 15:09:24 -0400

    Update streamlit_app.py
    Added the breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner!')
+
+//Breakfast menu
+streamlit.heading('Breakfast menu')
+streamlit.paragraph('Omega 3 & Blueberry Oatmeal')
+streamlit.paragraph('Kale, Spinach & Rocket Smoothie')
+streamlit.paragraph('Hard-Boiled, Free-Range Egg')
