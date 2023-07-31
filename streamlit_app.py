7fa1a0f833718b6534c4e98865ebd31a55bd7bfb
SnowflakeZKInterm <140736730+SnowflakeZKInterm@users.noreply.github.com>
2023-07-31 15:34:13 -0400

    Update streamlit_app.py
    Change it so the table show only fruits that have been selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,20 +3,24 @@ import pandas
 
 streamlit.title('My Parents New Healthy Diner!')
 
-#Breakfast menu 🥣 🥗 🐔 🥑🍞
+  #Breakfast menu 🥣 🥗 🐔 🥑🍞
 streamlit.header('Breakfast menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled, Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-#Special Header Smoothie
+  #Special Header Smoothie
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-# Display the table on the page.
-#Creating a pandas dataframe and display for it
+  # Display the table on the page.
+  #Creating a pandas dataframe and display for it
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-# Pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
+  # Pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+  #Dataframe display
 streamlit.dataframe(my_fruit_list)
