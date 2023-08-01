f8c851e7057c912836de432a7f76334ca205be75
SnowflakeZKInterm <140736730+SnowflakeZKInterm@users.noreply.github.com>
2023-08-01 14:03:40 -0400

    Update streamlit_app.py
    Normalized api call and added to a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,3 +32,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Normalize the api calls response 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Turn/display as the response into a dataframe
+streamlit.dataframe(fruityvice_normalized)
