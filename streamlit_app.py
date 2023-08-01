539de9d24557cd5ef9030a9a100e93f129f78bb6
SnowflakeZKInterm <140736730+SnowflakeZKInterm@users.noreply.github.com>
2023-08-01 14:23:27 -0400

    Update streamlit_app.py
    Testing something

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,12 +25,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
   #Dataframe display
 streamlit.dataframe(fruits_to_show)
 
+ #Text input for getting the user's fruit info
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
   #nEW SECTION FOR FRUITYVICE API RESPONSE
 streamlit.header('Fruityvice Fruit Advice!')
 
-  #Text input for getting the user's fruit info
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+ 
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
