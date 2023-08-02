42d1e9e1622ac43341141cd52ce3a0fb3fceafb8
SnowflakeZKInterm <140736730+SnowflakeZKInterm@users.noreply.github.com>
2023-08-02 14:35:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,9 +33,9 @@ streamlit.dataframe(fruits_to_show)
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
       # Normalize the api calls response 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       # Turn/display as the response into a dataframe
-    return fruityvice_normalized
+  return fruityvice_normalized
 
   #nEW SECTION FOR FRUITYVICE API RESPONSE
 streamlit.header('Fruityvice Fruit Advice!')
